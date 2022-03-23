b5d0a475b5d217559a87f4ac1d8901971164b265
icep0ps <tapsmuko@gmail.com>
2022-03-23 23:14:06 +0200

    added clear button and basic keyboard support

--- js/main.js
+++ js/main.js
@@ -4,44 +4,59 @@ const numbers = document.querySelectorAll('.numbers')
 const operators = document.querySelectorAll('.operators')
 const display = document.querySelector('.output')
 const equals = document.querySelector('.equals')
+const clear = document.querySelector('.clear')
 var displayNumbers = []
 var displayOperator = []
 
+
+
+window.addEventListener('keydown', e => {
+    var currentInput = document.querySelector(`button[data-key="${e.keyCode}"]`).textContent
+    displayNumbers.push(currentInput)
+    display.textContent = displayNumbers.join("")
+})
+
+
 buttons.forEach((button) => {
     button.addEventListener('click', e => {
-        var currentInput = button.innerText
+        var currentInput = button.textContent
         displayNumbers.push(currentInput)
-        display.innerText = displayNumbers.join("")
+        display.textContent = displayNumbers.join("")
     })
 })
 
 equals.addEventListener('click', e => {
-    let firstNumbers = displayNumbers.splice(0, displayNumbers.indexOf(displayOperator.toString()))//this istaking the display numbers array and taking all the numbers form 0 to the operator
-    let secondNumbers = displayNumbers.splice(displayNumbers.indexOf(displayOperator.toString()), displayNumbers.length - 1)//this takes the numbers from the operator to the last number in the array
-    let useroperator = secondNumbers.shift()
+    //this istaking the display numbers array and taking all the numbers form 0 to the operator
+    let firstNumbers = displayNumbers.splice(0, displayNumbers.indexOf(displayOperator.toString()))
+    //this takes the numbers from the operator to the last number in the array
+    let secondNumbers = displayNumbers.splice(displayNumbers.indexOf(displayOperator.toString()), displayNumbers.length - 1)
+    secondNumbers.shift()
     var numbers = [parseInt(firstNumbers.join("")), parseInt(secondNumbers.join(""))]
-    display.innerText = operator(displayOperator.toString(), numbers)
+    display.textContent = operator(displayOperator.toString(), numbers)
+    displayNumbers = [operator(displayOperator.toString(), numbers)]
+    displayOperator = []
+    console.log(displayNumbers)
 
 })
 
 operators.forEach((op) => {
     op.addEventListener('click', e => {
-        displayOperator.push(op.innerText) 
+        displayOperator.push(op.textContent)
         console.log(displayNumbers)
-        while(displayOperator.length>1){
+        while (displayOperator.length > 1) {
             console.log(displayOperator)
-           var currentOp = [displayOperator.shift()]
-           console.log(currentOp)
+            var currentOp = [displayOperator.shift()]
+            console.log(currentOp)
             console.log(displayNumbers)
-            var firstNumbers = displayNumbers.splice(0, displayNumbers.indexOf(currentOp.toString()))//this istaking the display numbers array and taking all the numbers form 0 to the operator
-            console.log(firstNumbers)       
-            var secondNumbers =displayNumbers.splice(displayNumbers.indexOf(currentOp.toString())+1, displayNumbers.length-2 )//this takes the numbers from the operator to the last number in the array
+            var firstNumbers = displayNumbers.splice(0, displayNumbers.indexOf(currentOp.toString()))
+            console.log(firstNumbers)
+            var secondNumbers = displayNumbers.splice(displayNumbers.indexOf(currentOp.toString()) + 1, displayNumbers.length - 2)
             console.log(secondNumbers)
             var numbers = [parseInt(firstNumbers.join("")), parseInt(secondNumbers.join(""))]
             displayNumbers = [operator(currentOp.toString(), numbers)].concat(displayOperator)
             console.log(displayNumbers)
 
-            
+
 
         }
 
@@ -50,46 +65,65 @@ operators.forEach((op) => {
 })
 
 
+
+clear.addEventListener('click', e => {
+    display.textContent = ''
+    displayNumbers = []
+    displayOperator = []
+})
+
+
+
+window.addEventListener('keydown', e => {
+    if (e.keyCode == 13) {
+        let firstNumbers = displayNumbers.splice(0, displayNumbers.indexOf(displayOperator.toString()))
+        let secondNumbers = displayNumbers.splice(displayNumbers.indexOf(displayOperator.toString()), displayNumbers.length - 1)
+        secondNumbers.shift()
+        var numbers = [parseInt(firstNumbers.join("")), parseInt(secondNumbers.join(""))]
+        display.textContent = operator(displayOperator.toString(), numbers)
+    }
+})
+
 function add(numbers) {
-    var answer = numbers.reduce((a, c) => 
-         a + c
+    var answer = numbers.reduce((a, c) =>
+        a + c
     );
-return answer
+    return answer
 };
 
 function subtract(numbers) {
-   var answer = numbers.reduce((a, c) => 
+    var answer = numbers.reduce((a, c) =>
         a - c
     );
     return answer
 };
 
 function multiply(numbers) {
-    var answer = numbers.reduce((a, c) => 
-         a * c
+    var answer = numbers.reduce((a, c) =>
+        a * c
     );
     return answer
 };
 
 function divided(numbers) {
-   var answer = numbers.reduce((a, c) => 
+    var answer = numbers.reduce((a, c) =>
         a / c
-);
+    );
     return answer
 };
 
 function operator(operator, numbers) {
     if (operator == '+') {
-          return add(numbers)
+        return add(numbers)
     }
     else if (operator == '-') {
-          return subtract(numbers)
+        return subtract(numbers)
     }
     else if (operator == 'x') {
-          return multiply(numbers)
+        return multiply(numbers)
     }
     else if (operator == '÷') {
-           return divided(numbers).toFixed(2)
+        return divided(numbers).toFixed(2)
     }
 }
 
